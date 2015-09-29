7b992041a5da6dd1ab8a8573f301a8932d6f8d11
Colin <cponce@cs.cornell.edu>
2015-09-29 11:04:06 -0400

    Quick update to README.md

--- Code/run_tests.py
+++ Code/run_tests.py
@@ -4,7 +4,7 @@ from itertools import chain
 
 # NAME = "FLiER_Substation_Test"
 # argdict = {}
-# argdict["-network"] = "../../test_networks/ieee57cdf.txt"
+# argdict["-network"] = "../Networks/ieee57cdf.txt"
 # argdict["-use_filter"] = "True"
 
 # argdict["-pmus"] = "3,12,33"
@@ -43,7 +43,7 @@ from itertools import chain
 
 # NAME = "FLiER_Test"
 # argdict = {}
-# argdict["-network"] = "../../test_networks/ieee57cdf.txt"
+# argdict["-network"] = "../Networks/ieee57cdf.txt"
 # argdict["-use_filter"] = "True"
 
 # argdict["-pmus"] = "3,12,33"
@@ -83,7 +83,7 @@ from itertools import chain
 
 # NAME = "FLiER_Test"
 # argdict = {}
-# argdict["-network"] = "../../test_networks/ieee57cdf.txt"
+# argdict["-network"] = "../Networks/ieee57cdf.txt"
 # argdict["-use_filter"] = "True"
 # argdict["-FLiER_type"] = "DC_Approximation"
 
@@ -127,7 +127,7 @@ from itertools import chain
 
 # NAME = "FLiER_Test"
 # argdict = {}
-# argdict["-network"] = "../../test_networks/ieee118cdf.txt"
+# argdict["-network"] = "../Networks/ieee118cdf.txt"
 # argdict["-use_filter"] = "True"
 
 # argdict["-pmus"] = "4,16,36,65,79,99"
@@ -170,35 +170,33 @@ from itertools import chain
 
 
 
-NAME = "FLiER_Test"
-argdict = {}
-argdict["-use_filter"] = "True"
-argdict["-network"] = "../../test_networks/case2383wp.m"
-argdict["-pmus"] = ("1127,961,2287,512,1496,1423,470,1804,515,207,1736,1525," + 
-                   "1769,1962,2237,1886,326,1403,1441,1437,208,292,1362,1905," + 
-                   "1354,909,2001,917,1304,801,1889,1993,997,441,1697,890," + 
-                   "1942,693,405,2038,2235,876,2160,1505,669,2239,1363,551," + 
-                   "1369,1860,186,1431,2333,1225,402,376,2165,992,831,2249," + 
-                   "65,1406,875,2073,683,527,1421,1917,1630,818,766,748,621," +
-                   "1710,39,1173,1202,535,1747,1952,1651,154,770,583,1740," + 
-                   "543,338,2296,2340,1035,2215,168,286,88,1857,1135,2241," + 
-                   "1018,803,180")
-argdict["-test_scenarios"] = "1351,1988,852,2878,1329,965,913,814,653,228"
-argdict["-write_file"] = "out.txt"
-argdict["-verbose"] = "True"
-args = [NAME] + list(chain(*argdict.iteritems()))
-FLiER_Test.main(args)
+# NAME = "FLiER_Test"
+# argdict = {}
+# argdict["-use_filter"] = "True"
+# argdict["-network"] = "../Networks/case2383wp.m"
+# argdict["-pmus"] = ("1127,961,2287,512,1496,1423,470,1804,515,207,1736,1525," + 
+#                    "1769,1962,2237,1886,326,1403,1441,1437,208,292,1362,1905," + 
+#                    "1354,909,2001,917,1304,801,1889,1993,997,441,1697,890," + 
+#                    "1942,693,405,2038,2235,876,2160,1505,669,2239,1363,551," + 
+#                    "1369,1860,186,1431,2333,1225,402,376,2165,992,831,2249," + 
+#                    "65,1406,875,2073,683,527,1421,1917,1630,818,766,748,621," +
+#                    "1710,39,1173,1202,535,1747,1952,1651,154,770,583,1740," + 
+#                    "543,338,2296,2340,1035,2215,168,286,88,1857,1135,2241," + 
+#                    "1018,803,180")
+# argdict["-test_scenarios"] = "1351,1988,852,2878,1329,965,913,814,653,228"
+# argdict["-write_file"] = "out.txt"
+# argdict["-verbose"] = "True"
+# args = [NAME] + list(chain(*argdict.iteritems()))
+# FLiER_Test.main(args)
 
-argdict["-use_filter"] = "False"
-args = [NAME] + list(chain(*argdict.iteritems()))
-FLiER_Test.main(args)
-
-assert False
+# argdict["-use_filter"] = "False"
+# args = [NAME] + list(chain(*argdict.iteritems()))
+# FLiER_Test.main(args)
 
 NAME = "FLiER_Substation_Test"
 argdict = {}
 argdict["-use_filter"] = "True"
-argdict["-network"] = "../../test_networks/case2383wp.m"
+argdict["-network"] = "../Networks/case2383wp.m"
 argdict["-pmus"] = ("1127,961,2287,512,1496,1423,470,1804,515,207,1736,1525," + 
                    "1769,1962,2237,1886,326,1403,1441,1437,208,292,1362,1905," + 
                    "1354,909,2001,917,1304,801,1889,1993,997,441,1697,890," + 
@@ -214,13 +212,11 @@ argdict["-test_scenarios"] = ("86,1 2 3;176,1 2;539,7;702,4 5;754,2 3 4 5;" +
 argdict["-write_file"] = "Polishsubs_sparse35.txt"
 argdict["-verbose"] = "True"
 args = [NAME] + list(chain(*argdict.iteritems()))
-# FLiER_Substation_Test.main(args)
 
 # argdict["-use_filter"] = "False"
 # args = [NAME] + list(chain(*argdict.iteritems()))
 # FLiER_Substation_Test.main(args)
 
 argdict["-use_filter"] = "True"
-#del argdict["-test_scenarios"]
 args = [NAME] + list(chain(*argdict.iteritems()))
 FLiER_Substation_Test.main(args)
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # FLiER_Test_Suite
+
+This repository contains code to solve power flow equations, as well as to run
+the FLiER algorithms discussed in our paper.
+
+Some example python calls, to be run in the /Code directory, are the following:
+
+
+python FLiER_Test.py -network ../Networks/ieee57cdf.txt -pmus 3,12,33 -use_filter True -noise 0.0 -write_file output.txt
+
+python FLiER_Substation_Test.py -network ../Networks/ieee57cdf.txt -pmus 3,12,33 -use_filter True -noise 0.0 -test_type Full -write_file output.txt
+
+python FLiER_Substation_Test.py -network ../Networks/ieee57cdf.txt -pmus 3,12,33 -use_filter True -noise 0.0 -test_type Single_Lines -write_file output.txt
+
+
+Note that our code 0-indexes network buses, while e.g. MATLAB 1-indexes buses. So, for example, placing PMUs on 0-indexed buses 3, 12, and 33 means placing PMUs on 1-indexed buses 4, 13, and 34.
