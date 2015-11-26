56c99a48bc2a61bbe3dddb21305412dbf9a1868c
Colin <cponce@cs.cornell.edu>
2015-11-26 11:59:33 -0500

    Added a license notice

--- Code/run_tests.py
+++ Code/run_tests.py
@@ -1,3 +1,22 @@
+"""This file is part of the FLiER Test Suite.
+
+    The FLiER Test Suite is free software: you can redistribute it 
+	and/or modify it under the terms of the GNU General Public License as 
+	published by the Free Software Foundation, either version 3 of the 
+	License, or (at your option) any later version.
+
+    The FLiER Test Suite is distributed in the hope that it will be 
+	useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with the FLiER Test Suite.  If not, see 
+	<http://www.gnu.org/licenses/>.
+	
+	Copyright Colin Ponce 2015.
+"""
+
 import FLiER_Substation_Test
 import FLiER_Test
 from itertools import chain
